9121dafa59c0a38eb5f7292ec188cecd47da556c
Niels1006 <hermsen.niels@gmail.com>
2025-03-04 20:36:35 +0100

    improved including of scripts etc in user index.html - now works (adds tags in <head> of index.html to actual head of site and adds <body> to <body> of actual site

--- src/static/javascript/injections/setup.js
+++ src/static/javascript/injections/setup.js
@@ -1,65 +1,65 @@
 //check if in iframe 
 if (window.location !== window.parent.location) {
-  //exit this script 
-  return
+    //exit this script 
+    return
 }
 
 const head = document.getElementsByTagName('head')[0]
 
 const script_locations = [
-  "internal/static/javascript/main.js",
+    "internal/static/javascript/main.js",
 ];
 
 const css_locations = [
-  "internal/static/css/alwaysLoad.css",
+    "internal/static/css/alwaysLoad.css",
 ];
 
 if (notebookHidden === true) {
-  css_locations.push("internal/static/css/internal.css")
+    css_locations.push("internal/static/css/internal.css")
 }
 
 const url = "http://localhost:8085/"
 
 //wait for fileserver to be ready
 const interval = setInterval(async () => {
-  try {
-    const response = await fetch(url, { method: "HEAD" });
-    if (response.status == 200) {
-      clearInterval(interval)
+    try {
+        const response = await fetch(url, { method: "HEAD" });
+        if (response.status == 200) {
+            clearInterval(interval)
 
-      // add internal scripts
-      script_locations.forEach(location => {
-        const script = document.createElement('script')
-        script.src = url + location
-        script.type = "module"
-        head.appendChild(script)
-      });
+            // add internal scripts
+            script_locations.forEach(location => {
+                const script = document.createElement('script')
+                script.src = url + location
+                script.type = "module"
+                head.appendChild(script)
+            });
 
-      //add internal css
-      css_locations.forEach(location => {
-        const link = document.createElement('link')
-        link.href = url + location
-        link.rel = "stylesheet"
-        head.appendChild(link)
-      });
+            //add internal css
+            css_locations.forEach(location => {
+                const link = document.createElement('link')
+                link.href = url + location
+                link.rel = "stylesheet"
+                head.appendChild(link)
+            });
 
-      //add main script 
-      const script = document.createElement('script')
-      script.src = url + "javascript/main.js"
-      script.type = "module"
-      head.appendChild(script)
+            //add main script 
+            const script = document.createElement('script')
+            script.src = url + "javascript/main.js"
+            script.type = "module"
+            head.appendChild(script)
 
-      //add plugin scripts
-      for (let i = 0; i < js_files_to_load.length; i++) {
-        const script = document.createElement('script')
-        script.type = "module"
-        script.src = url + "absolute/" + js_files_to_load[i]
-        head.appendChild(script)
-      }
+            //add plugin scripts
+            for (let i = 0; i < js_files_to_load.length; i++) {
+                const script = document.createElement('script')
+                script.type = "module"
+                script.src = url + "absolute/" + js_files_to_load[i]
+                head.appendChild(script)
+            }
+        }
     }
-  }
-  catch {
-  }
+    catch {
+    }
 
 
 }, 100)
--- src/static/javascript/loadStatic.js
+++ src/static/javascript/loadStatic.js
@@ -2,18 +2,47 @@ import { callJuliaFunction } from './interface.js';
 
 export async function insertHTMLToBody() {
     const body = document.querySelector('body');
+    const head = document.querySelector('head');
 
     const mainSite = await ((await fetch("http://localhost:8085/index.html")).text());
+
+    //get scripts within mainSite and add them to head (Scripts are not executed when inside html added with insertAdjacentHTML)
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(mainSite, 'text/html');
+
+    // adds all scripts to the head
+    const scripts = doc.querySelectorAll('script');
+    scripts.forEach(script => {
+        const newScript = document.createElement('script');
+        newScript.src = script.src;
+        newScript.type = script.type;
+        head.appendChild(newScript);
+        script.remove();
+    });
+
+    //adds all links to the head
+    const links = doc.querySelectorAll('link');
+    links.forEach(link => {
+        const newLink = document.createElement('link');
+        newLink.href = link.href;
+        newLink.rel = link.rel;
+        head.appendChild(newLink);
+        link.remove();
+    });
+
+    //move rest of head to current head, like title and meta tags
+    head.insertAdjacentHTML('afterbegin', doc.querySelector('head').innerHTML);
+
+    //get all of body from mainSite and add it to the current body
+    body.insertAdjacentHTML('afterbegin', doc.querySelector('body').innerHTML);
+
+
+    //get settings html
     const settingsSite = await ((await fetch("http://localhost:8085/internal/static/html/settings.html")).text());
 
-    const htmlString = settingsSite + mainSite;
+    body.insertAdjacentHTML('afterbegin', settingsSite);
 
-    if (!document.getElementById('main-export')) {
-        body.insertAdjacentHTML('afterbegin', htmlString);
-    } else {
-        document.getElementById('main-export').remove();
-        body.insertAdjacentHTML('afterbegin', htmlString);
-    }
+
 }
 
 export async function addCSSToBody() {
