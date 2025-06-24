c7526c970f1dbaaa56b15e35378a1ef5939c0154
Niels1006 <hermsen.niels@gmail.com>
2025-06-24 20:46:31 +0200

    fix

--- src/static/javascript/logger.js
+++ src/static/javascript/logger.js
@@ -1,11 +1,26 @@
+/**
+ * Logs an informational message to the console with the PlutoBoard.jl prefix
+ * @param {string} message - The message to be logged
+ * @returns {void}
+ */
 export function info(message) {
     console.log(`[PlutoBoard.jl] ${message}`);
 }
 
+/**
+ * Logs a warning message to the console with the PlutoBoard.jl prefix
+ * @param {string} message - The warning message to be logged
+ * @returns {void}
+ */
 export function warn(message) {
     console.warn(`[PlutoBoard.jl] ${message}`);
 }
 
+/**
+ * Logs an error message to the console with the PlutoBoard.jl prefix
+ * @param {string} message - The error message to be logged
+ * @returns {void}
+ */
 export function error(message) {
     console.error(`[PlutoBoard.jl] ${message}`);
 }
