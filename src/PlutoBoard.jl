084292823272daf828afa42832f5ebcef3b448b0
Niels1006 <hermsen.niels@gmail.com>
2025-09-08 00:36:07 +0200

    more docs

--- src/PlutoBoard.jl
+++ src/PlutoBoard.jl
@@ -28,23 +28,33 @@ include("plugins/LoadPlugin.jl")
 include("HotReload.jl")
 include("Expressions.jl")
 
+"""
+    __init__()
 
+Initialize the PlutoBoard module by setting up global variables and configuration.
+
+This function is automatically called when the module is loaded. It sets up:
+- `SERVE_DIR`: The directory to serve static files from (defaults to "static" in current working directory)
+- `config`: Configuration loaded from "user_config.toml" if it exists in the current directory
+
+If no user_config.toml file is found, only the setup() function will be available.
+"""
 function __init__()
-	# Needs to be in init, so it runs every time PlutoBoard is loaded rather than only when PlutoBoard is being precompiled.
-	global SERVE_DIR = joinpath(pwd(), "static")
+    # Needs to be in init, so it runs every time PlutoBoard is loaded rather than only when PlutoBoard is being precompiled.
+    global SERVE_DIR = joinpath(pwd(), "static")
 
-	if isfile(joinpath(pwd(), "user_config.toml")) == true
-		global config = TOML.parsefile(joinpath(pwd(), "user_config.toml"))
-	else
-		@info "No user_config.toml file found in current directory. Only setup() will be available."
-	end
+    if isfile(joinpath(pwd(), "user_config.toml")) == true
+        global config = TOML.parsefile(joinpath(pwd(), "user_config.toml"))
+    else
+        @info "No user_config.toml file found in current directory. Only setup() will be available."
+    end
 end
 
 plutoboard_filepath = dirname(dirname(pathof(PlutoBoard)))
 
 
-html_path::Union{String, Nothing} = nothing
-css_path::Union{String, Nothing} = nothing
+html_path::Union{String,Nothing} = nothing
+css_path::Union{String,Nothing} = nothing
 hide_notebook::Bool = true
 
 scripts_urls::Array{String} = []
--- src/Utilities.jl
+++ src/Utilities.jl
@@ -10,10 +10,10 @@ export open_file, copy_with_delete, setup, get_package_name
 Opens a file and returns its content as a string.
 """
 function open_file(path::String)::String
-	f = open(path, "r")
-	content = read(f, String)
-	close(f)
-	return content
+    f = open(path, "r")
+    content = read(f, String)
+    close(f)
+    return content
 end
 
 
@@ -26,9 +26,9 @@ end
 Copies a file from one location to another and deletes the original file. Sets the permissions of the copied file to 666.
 """
 function copy_with_delete(from::String, to::String)
-	cp(from, to, force = true)
-	chmod(to, 0o766)
-	@info "Copied $from to $to"
+    cp(from, to, force=true)
+    chmod(to, 0o766)
+    @info "Copied $from to $to"
 end
 
 """
@@ -37,52 +37,52 @@ end
 Sets up PlutoBoard project by creating a new Julia package and copying necessary files.
 """
 function setup()
-	@info "Setting up PlutoBoard"
+    @info "Setting up PlutoBoard"
 
-	print("What should be the package name? ")
-	package_name = readline()
-	Pkg.generate(package_name)
+    print("What should be the package name? ")
+    package_name = readline()
+    Pkg.generate(package_name)
 
-	package_path = joinpath(pwd(), package_name)
-	package_file_path = joinpath(package_path, "src/$(package_name).jl")
+    package_path = joinpath(pwd(), package_name)
+    package_file_path = joinpath(package_path, "src/$(package_name).jl")
 
-	options = ["Bare", "Simple", "JuliaCon"]
-	menu = RadioMenu(options, pagesize = 3)
-	choice = request("Choose your setup template. \"Simple\" is recommended, \"JuliaCon\" let's you see what's possible. ", menu)
+    options = ["Bare", "Simple", "JuliaCon"]
+    menu = RadioMenu(options, pagesize=3)
+    choice = request("Choose your setup template. \"Simple\" is recommended, \"JuliaCon\" let's you see what's possible. ", menu)
 
-	examples_path = joinpath(dirname(dirname(Pkg.pathof(PlutoBoardExamples))), options[choice])
+    examples_path = joinpath(dirname(dirname(Pkg.pathof(PlutoBoardExamples))), options[choice])
 
 
-	# copy everything into `package_path`
-	for file in readdir(examples_path)
-		if file == "Project.toml"
-			# only add [deps] and [compat] sections from the example Project.toml
-			example_toml = TOML.parsefile(joinpath(examples_path, file))
-			new_toml = TOML.parsefile(joinpath(package_path, file))
-			for (k, v) in example_toml
-				if k in ["deps", "compat"]
-					new_toml[k] = v
-				end
-			end
-			open(joinpath(package_path, file), "w") do io
-				TOML.print(io, new_toml)
-			end
-			continue
-		end
+    # copy everything into `package_path`
+    for file in readdir(examples_path)
+        if file == "Project.toml"
+            # only add [deps] and [compat] sections from the example Project.toml
+            example_toml = TOML.parsefile(joinpath(examples_path, file))
+            new_toml = TOML.parsefile(joinpath(package_path, file))
+            for (k, v) in example_toml
+                if k in ["deps", "compat"]
+                    new_toml[k] = v
+                end
+            end
+            open(joinpath(package_path, file), "w") do io
+                TOML.print(io, new_toml)
+            end
+            continue
+        end
 
-		cp(joinpath(examples_path, file), joinpath(package_path, file); force = true)
-	end
+        cp(joinpath(examples_path, file), joinpath(package_path, file); force=true)
+    end
 
-	# change options[choice] in `src/$options[choice].jl` to `package_name`
-	content = read(joinpath(package_path, "src", options[choice] * ".jl"), String)
-	fixed_content = replace(content, "$(options[choice])" => "$(package_name)")
-	rm(joinpath(package_path, "src", options[choice] * ".jl"))
-	write(joinpath(package_path, "src", package_name * ".jl"), fixed_content)
+    # change options[choice] in `src/$options[choice].jl` to `package_name`
+    content = read(joinpath(package_path, "src", options[choice] * ".jl"), String)
+    fixed_content = replace(content, "$(options[choice])" => "$(package_name)")
+    rm(joinpath(package_path, "src", options[choice] * ".jl"))
+    write(joinpath(package_path, "src", package_name * ".jl"), fixed_content)
 
-	# copy config/user_config.toml to package_path/user_config.toml
-	cp(joinpath(PlutoBoard.plutoboard_filepath, "config", "user_config.toml"), joinpath(package_path, "user_config.toml"); force = true)
+    # copy config/user_config.toml to package_path/user_config.toml
+    cp(joinpath(PlutoBoard.plutoboard_filepath, "config", "user_config.toml"), joinpath(package_path, "user_config.toml"); force=true)
 
-	@info "Setup complete"
+    @info "Setup complete"
 end
 
 
@@ -92,43 +92,57 @@ end
 Returns the name of the package from the Project.toml file.
 """
 function get_package_name()::String
-	project_toml_path = joinpath(pwd(), "Project.toml")
-	project_toml = TOML.parsefile(project_toml_path)
-	return project_toml["name"]
+    project_toml_path = joinpath(pwd(), "Project.toml")
+    project_toml = TOML.parsefile(project_toml_path)
+    return project_toml["name"]
 end
 
-function get_local_user_config()::Dict{String, Any}
-	user_config_path = joinpath(pwd(), "user_config.toml")
-	if isfile(user_config_path)
-		return TOML.parsefile(user_config_path)
-	else
-		return Dict{String, Any}()
-	end
+
+"""
+	get_local_user_config()::Dict{String, Any}
+Reads the user_config.toml file from the current directory and returns it as a dictionary. If the file does not exist, returns an empty dictionary.
+"""
+function get_local_user_config()::Dict{String,Any}
+    user_config_path = joinpath(pwd(), "user_config.toml")
+    if isfile(user_config_path)
+        return TOML.parsefile(user_config_path)
+    else
+        return Dict{String,Any}()
+    end
 end
 
-function update_local_user_config(new_config::Dict{String, Any})
-	user_config_path = joinpath(pwd(), "user_config.toml")
-	if isfile(user_config_path)
-		current_config = TOML.parsefile(user_config_path)
-		merged_config = merge(current_config, new_config)
-		open(user_config_path, "w") do io
-			TOML.print(io, merged_config)
-		end
-	else
-		open(user_config_path, "w") do io
-			TOML.print(io, new_config)
-		end
-	end
+"""
+	update_local_user_config(new_config::Dict{String,Any})::nothing
+Updates the user_config.toml file in the current directory with the new configuration provided in `new_config`. If the file does not exist, it will be created.
+"""
+function update_local_user_config(new_config::Dict{String,Any})
+    user_config_path = joinpath(pwd(), "user_config.toml")
+    if isfile(user_config_path)
+        current_config = TOML.parsefile(user_config_path)
+        merged_config = merge(current_config, new_config)
+        open(user_config_path, "w") do io
+            TOML.print(io, merged_config)
+        end
+    else
+        open(user_config_path, "w") do io
+            TOML.print(io, new_config)
+        end
+    end
 
-	PlutoBoard.config = TOML.parsefile(user_config_path)
+    PlutoBoard.config = TOML.parsefile(user_config_path)
 end
 
+
+"""
+	is_port_free(port::Int)::Bool
+Checks if a port is free to use. Returns true if the port is free, false otherwise.
+"""
 function is_port_free(port::Int)::Bool
-	try
-		server = listen(IPv4("127.0.0.1"), port)
-		close(server)
-		return true
-	catch e
-		return false
-	end
+    try
+        server = listen(IPv4("127.0.0.1"), port)
+        close(server)
+        return true
+    catch e
+        return false
+    end
 end
